d0b4bea238c52eb0ab45a419139271f616750daf
tjasika <tjasazzeleznik@gmail.com>
2026-04-27 21:33:29 +0200

    feat: enable settings form and local storage

--- src/app/pages/settings/settings.ts
+++ src/app/pages/settings/settings.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
-export class Settings {}
+export class Settings {
+  clientId = '';
+  clientSecret = '';
+  tokenUrl = '';
+  baseUrl = '';
+
+  ngOnInit() {
+    this.clientId = localStorage.getItem('clientId') || '';
+    this.clientSecret = localStorage.getItem('clientSecret') || '';
+    this.tokenUrl = localStorage.getItem('tokenUrl') || '';
+    this.baseUrl = localStorage.getItem('baseUrl') || '';
+  }
+
+  saveSettings() {
+    localStorage.setItem('clientId', this.clientId);
+    localStorage.setItem('clientSecret', this.clientSecret);
+    localStorage.setItem('tokenUrl', this.tokenUrl);
+    localStorage.setItem('baseUrl', this.baseUrl);
+    alert('Settings saved!');
+  }
+}
