00699c7c478ed0d8b486dcf68248581cabc4e19d
tjasika <tjasazzeleznik@gmail.com>
2026-04-27 20:30:25 +0200

    feat: enable routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Settings } from './pages/settings/settings';
+import { Users } from './pages/users/users';
+import { Absences } from './pages/absences/absences';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+  { path: 'users', component: Users },
+  { path: 'settings', component: Settings },
+  { path: 'absences', component: Absences },
+];
