ac4b4e4a80a2b1b41db0eda0038946bf05ba9cb1
tjasika <tjasazzeleznik@gmail.com>
2026-04-30 20:26:38 +0200

    fix: copy methods from users.ts to absences.ts, filter by date

--- src/app/pages/absences/absences.ts
+++ src/app/pages/absences/absences.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Api } from '../../services/api';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-absences',
-  imports: [],
+  imports: [FormsModule, DatePipe],
   templateUrl: './absences.html',
   styleUrl: './absences.css',
 })
-export class Absences {}
+export class Absences implements OnInit {
+  absences: any[] = [];
+  users: any[] = [];
+  absenceDefinitions: any[] = [];
+  selectedDate = '';
+  isLoading = true;
+
+  constructor(private api: Api, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.api.getToken().subscribe({
+      next: () => {
+        this.api.getAbsences().subscribe({
+          next: () => {
+            this.absences = this.api.absences;
+            this.isLoading = false;
+            this.cdr.detectChanges();
+          },
+          error: (err) => console.error('Error fetching absences:', err)
+        });
+        this.api.getUsers().subscribe({
+          next: () => {
+            this.users = this.api.users;
+            this.cdr.detectChanges();
+          }
+        });
+        this.api.getAbsenceDefinitions().subscribe({
+          next: () => {
+            this.absenceDefinitions = this.api.absenceDefinitions;
+            this.cdr.detectChanges();
+          }
+        });
+      },
+      error: (err) => console.error('Token failed:', err)
+    })
+  }
+
+  getUserAbsences(userId: string) {
+    if(!this.selectedDate) return [];
+    const selected = new Date(this.selectedDate);
+    return this.absences.filter(a => {
+      const from = new Date(a.PartialTimeFrom);
+      const to = new Date(a.PartialTimeTo);
+      return a.UserId === userId && selected >= from && selected <= to;
+    });
+  }
+
+  get filteredUsers() {
+    if(!this.selectedDate) return [];
+    return this.users.filter(u => this.getUserAbsences(u.Id).length > 0);
+  }
+
+  refresh() {
+    window.location.reload();
+  }
+
+}
